d3c340e7d681e94fc1aa808fdfe8d6181d79ca13
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2010-04-08 15:28:26 +0900

    added simple gettext sample

---
+++ src/c++/misc/gettext/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <libintl.h>
+#include <locale.h>
+using namespace std;
+#define _(String) gettext (String)
+
+int main()
+{
+  setlocale(LC_ALL, "");
+  if (bindtextdomain("main", "./locale") == NULL) {
+    cerr << "damepo" << endl;
+    return -1;
+  }
+  textdomain("main");
+
+  cout << _("Hello world!") << endl;
+  cout << _("hoge") << endl;
+  cout << _("moge") << endl;
+  return 0;
+}
